ed6751cd6c7a458874991bdf9af3fd8b93a4bdfd
dimonhacker777 <zhakin.dimitriy@gmail.com>
2026-09-21 10:05:34 +0300

    gfdrtg

--- rabota-s-git/app.ts
+++ rabota-s-git/app.ts
@@ -1,1 +1,4 @@
-export const add = (a: number, b: number): number => a + b;
+export const divide = (a: number, b: number): number => {
+    if (b === 0) throw new Error('Деление на 0');
+    return a / b;
+}
